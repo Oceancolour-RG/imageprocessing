128e96adee7eb018bafca3ca095e0cb435fcd228
oceancolour-rg <garciara.0@gmail.com>
2022-06-01 15:55:51 +0800

    improved code

--- micasense/load_yaml.py
+++ micasense/load_yaml.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
 import yaml
-from pathlib2 import Path
-from typing import Union
+from pathlib import Path
+from typing import Optional, Union
 
 
-def load_all(yaml_file: Union[Path, str]) -> dict:
+def load_all(yaml_file: Union[Path, str], key: Optional[str] = None) -> Union[dict, None]:
+    # open the yaml file
+    val = None
     with open(yaml_file, "r") as fid:
-        d = yaml.safe_load(fid)  # dict
-    return d
+        if key:
+            for i, row in enumerate(fid):
+                if key in row:
+                    vdict = yaml.safe_load(row)
+                    val = vdict[key]
+                    break
+        else:
+            # read yaml contents to a dict
+            val = yaml.safe_load(fid)
+
+    return val
